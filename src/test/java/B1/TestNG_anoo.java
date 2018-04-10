994be11610cd216fbecb0afde70ab59a56d83773
Ajitdarbhe <darbheajit@gmail.com>
2018-04-10 13:40:11 +0530

    TestNGanno classs addded

---
+++ src/test/java/B1/TestNG_anoo.java
@@ -0,0 +1,17 @@
+package B1;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+
+public class TestNG_anoo 
+{
+	WebDriver d;
+	
+	@BeforeMethod
+	public void before() 
+	{
+		
+		
+	}
+
+}
