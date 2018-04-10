6dcf95d639f81f7106943678b1ce6a7e48245b55
Ajitdarbhe <darbheajit@gmail.com>
2018-04-10 15:02:51 +0530

    Data driven class added

---
+++ src/test/java/B1/datadriven_testng.java
@@ -0,0 +1,11 @@
+package B1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class datadriven_testng 
+{
+	System.setProperty("webdriver.chrome.driver", "D:/Ajit_Selenium_Drivers/chromedriver.exe");
+	
+	WebDriver d=new ChromeDriver(); 
+}
